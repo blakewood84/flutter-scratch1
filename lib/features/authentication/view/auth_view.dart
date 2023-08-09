5851fc27a2eacd8e0ba73a6d181890d98194f729
Blake Wood Jr <blakewoodjr84@gmail.com>
2023-08-08 20:46:35 -0700

    Updated UI for phone number login

--- lib/features/authentication/view/auth_view.dart
+++ lib/features/authentication/view/auth_view.dart
@@ -24,18 +24,25 @@ class _AuthViewState extends State<AuthView> {
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      // if (showPhoneAuth) Icon(Icons.chevron_left),
                       AnimatedContainer(
-                        width: showPhoneAuth ? 20 : 100,
-                        height: showPhoneAuth ? 20 : 100,
+                        width: showPhoneAuth ? 0 : 0,
+                        height: showPhoneAuth ? 40 : 100,
                         curve: Curves.easeInOut,
-                        duration: const Duration(milliseconds: 200),
+                        duration: const Duration(milliseconds: 400),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              width: 1,
+                              color: Colors.red,
+                            ),
+                          ),
+                        ),
                       ),
                       AnimatedContainer(
                         width: showPhoneAuth ? 130 : 300,
                         height: showPhoneAuth ? 130 : 300,
                         duration: const Duration(seconds: 1),
-                        curve: Curves.fastLinearToSlowEaseIn,
+                        curve: Curves.fastEaseInToSlowEaseOut,
                         decoration: BoxDecoration(
                           border: Border.all(
                             width: 1,
@@ -44,11 +51,11 @@ class _AuthViewState extends State<AuthView> {
                         ),
                       ),
                       if (showPhoneAuth) ...[
-                        const SizedBox(height: 10),
+                        const SizedBox(height: 20),
                         const Text(
                           'Enter your phone number',
                           style: TextStyle(
-                            fontSize: 14,
+                            fontSize: 20,
                             fontWeight: FontWeight.w700,
                           ),
                         ),
@@ -59,16 +66,14 @@ class _AuthViewState extends State<AuthView> {
                             'Just for verification. We won\'t call you or give it to anyone.',
                             textAlign: TextAlign.center,
                             style: TextStyle(
-                              fontSize: 12,
-                              color: Colors.grey.shade600,
+                              fontSize: 14,
+                              color: Color(0xff999999),
                             ),
                           ),
                         ),
-                        const TextField(
-                          autofocus: true,
-                        ),
+                        const SizedBox(height: 20),
+                        const _PhoneNumberInput()
                       ],
-
                       const Spacer(),
                       if (!showPhoneAuth)
                         Container(
@@ -91,7 +96,7 @@ class _AuthViewState extends State<AuthView> {
                             backgroundColor: Colors.white,
                             foregroundColor: Colors.black,
                             padding: const EdgeInsets.symmetric(
-                              vertical: 15,
+                              vertical: 20,
                             ),
                           ),
                           onPressed: () {
@@ -101,7 +106,7 @@ class _AuthViewState extends State<AuthView> {
                           },
                           child: Text(
                             !showPhoneAuth ? 'Enter' : 'Next',
-                            style: TextStyle(
+                            style: const TextStyle(
                               fontWeight: FontWeight.w700,
                               color: Colors.black,
                             ),
@@ -126,7 +131,7 @@ class _AuthViewState extends State<AuthView> {
                   opacity: showPhoneAuth ? 1 : 0,
                   duration: const Duration(milliseconds: 300),
                   curve: Curves.fastEaseInToSlowEaseOut,
-                  child: Icon(
+                  child: const Icon(
                     CupertinoIcons.chevron_left,
                     size: 20,
                   ),
@@ -140,11 +145,55 @@ class _AuthViewState extends State<AuthView> {
   }
 }
 
-class NextButton extends StatelessWidget {
-  const NextButton({super.key});
+class _PhoneNumberInput extends StatelessWidget {
+  const _PhoneNumberInput({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ConstrainedBox(
+      constraints: const BoxConstraints(
+        maxHeight: 50,
+      ),
+      child: Row(
+        children: [
+          Container(
+            width: 60,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15.0),
+              color: const Color(0xff444444),
+            ),
+          ),
+          const SizedBox(width: 10),
+          Expanded(
+            child: TextField(
+              autofocus: true,
+              keyboardType: TextInputType.phone,
+              decoration: InputDecoration(
+                isDense: true,
+                hintText: 'Enter Phone Number',
+                hintStyle: const TextStyle(
+                  color: Colors.white,
+                ),
+                filled: true,
+                fillColor: const Color(0xff444444).withOpacity(.9),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                  borderSide: BorderSide.none,
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                  borderSide: BorderSide.none,
+                ),
+                errorStyle: const TextStyle(
+                  fontSize: 0,
+                  height: 0,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
