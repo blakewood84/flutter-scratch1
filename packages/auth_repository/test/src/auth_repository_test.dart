a18012108e6ddcf03e6c37400e3710af50fbdcf1
Blake Wood Jr <blakewoodjr84@gmail.com>
2023-08-23 13:40:08 -0700

    Updated to add test.

--- packages/auth_repository/test/src/auth_repository_test.dart
+++ packages/auth_repository/test/src/auth_repository_test.dart
@@ -1,9 +1,14 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:auth_repository/auth_repository.dart';
+import 'package:test/test.dart';
+
 void main() {
-  // group('AuthRepository', () {
-  //   test('can be instantiated', () {
-  //     expect(AuthRepository(), isNotNull);
-  //   });
-  // });
+  final authRepository = AuthRepository();
+
+  group('AuthRepository', () {
+    test('can be instantiated', () {
+      expect(authRepository, isNotNull);
+    });
+  });
 }
